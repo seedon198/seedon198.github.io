b34750170b4d5624b6e00f3b9ae7915ff922cb57
seedon198 <adlin.seedon@learner.manipal.edu>
2020-02-10 18:09:12 +0530

    update

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed=3;
-Typer.file="[your-name].txt"; // add your own name here
+Typer.file="seedon198.txt";
 Typer.init();
  
 var timer = setInterval("t();", 30);

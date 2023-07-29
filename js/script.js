479074f633b975276862a87717fba9f9e0ea4247
seedon198 <adlin.seedon@learner.manipal.edu>
2023-07-30 01:27:11 +0530

    Automatic commit at 2023-07-30 01:27:11 Modified files: js/script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 //if (window.location.protocol !== 'https:') {window.location.href = 'https://' + window.location.host + window.location.pathname;}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change slide every 2 seconds (adjust as needed)
+}

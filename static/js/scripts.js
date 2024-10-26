5a708e525c76a036e9dc51b499bdcb526d02f579
seedon198 <adlin.seedon@learner.manipal.edu>
2024-10-27 01:27:39 +0530

    modified:   static/js/scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -391,3 +391,9 @@ if (document.readyState === 'complete') {
     initializeModal();
     initializeParticles();
 }
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YC8JWDXN1J');
